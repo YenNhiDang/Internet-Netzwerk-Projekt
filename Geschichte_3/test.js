4cf82a3d36536439e77bd9402c43363ba9afa58a
komplett-anders <jonasvanloh@gmail.com>
2025-12-03 17:23:30 +0100

    javascript qeullen button test

---
+++ Geschichte_3/test.js
@@ -0,0 +1,4 @@
+document.getElementById("button").addEventListener("click", () => {
+    const section = document.getElementById("toggle");
+    section.style.display = section.style.display === "none" ? "block" : "none";
+});
