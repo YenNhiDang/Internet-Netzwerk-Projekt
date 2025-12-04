1c351d69f3c6ce0cfef4e978068c8ffb2a566ceb
komplett-anders <jonasvanloh@gmail.com>
2025-12-04 18:03:41 +0100

    verbesserung der buttons

--- Geschichte_3/test.js
+++ Geschichte_3/test.js
@@ -1,4 +1,9 @@
-document.getElementById("button").addEventListener("click", () => {
-    const section = document.getElementById("toggle");
-    section.style.display = section.style.display === "none" ? "block" : "none";
+document.querySelectorAll(".button").forEach(button => {
+    button.addEventListener("click", () => {
+        const targetId = button.dataset.target;    
+        const section = document.getElementById(targetId);
+
+        section.style.display = 
+            section.style.display === "none" ? "block" : "none";
+    });
 });
