a9eaa39483ad7166badd000648d4134efcbae391
UnkreativLeonie <leonie.schuchardt@stud.hs-emden-leer.de>
2025-12-06 02:26:56 +0100

    Hamburger
    Habe die Navigation mit dem Hamburger-Icon bei allen hinzugefügt.

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ document.querySelectorAll(".button").forEach(button => {
             section.style.display === "none" ? "block" : "none";
     });
 });
+
+/* Hamburger */
+// Elemente auswählen
+const hamburger = document.getElementById('hamburger');
+const sidebar = document.querySelector('.sidebar');
+
+// Klick-Funktion für #hamburger hinzufügen
+hamburger.addEventListener('click', navStatus);
+
+// Prüfen ob die Navigation geöffnet oder geschlossen ist
+function navStatus() {
+  if (document.body.classList.contains('hamburger-active')) {
+    navClose();
+  } else {
+    navOpen();
+  }
+}
+
+// Navi schließen
+function navClose() {
+  document.body.classList.remove('hamburger-active');
+  sidebar.classList.remove('open');  // <--- Sidebar verstecken
+}
+
+// Navi öffnen
+function navOpen() {
+  document.body.classList.add('hamburger-active');
+  sidebar.classList.add('open'); // <--- Sidebar anzeigen
+}
